efa4ed3a29a843b705407d2ae0579a41f475bbb3
Vtsl-patel <codephysicsvatsal@gmail.com>
2024-06-20 18:43:27 +0530

    appwrite services added

---
+++ src/appwrite/configuration.js
@@ -0,0 +1,137 @@
+import config from "../config/config";
+import { Client, ID, Databases, Storage, Query } from "appwrite";
+
+export class Service {
+    client = new Client();
+    databases;
+    storage;
+
+    constructor() {
+        this.client
+                .setEndpoint(config.appwriteUrl)
+                .setProject(config.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    async createPost({title, slug, content, featuredImage, status, userId}) {
+        try {
+            return await this.databases.createDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug, // documentID (we could also use ID.unique())
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error ", error);
+        }
+    }
+
+    async updatePost(slug, {title, content, featuredImage, statu}) {
+        try {
+            return await this.databases.updateDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug, // documentID (we could also use ID.unique())
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: updatePost :: error ", error);
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug // documentID (we could also use ID.unique())
+            )
+            return true
+        } catch (error) {
+            console.log("Appwrite service :: deletePost :: error ", error);
+            return false
+        }
+    }
+
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                slug // documentID (we could also use ID.unique())
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPost :: error ", error);
+            return false
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status", "active")]){
+        try {
+            return await this.databases.listDocuments(
+                config.appwriteDatabaseId,
+                config.appwriteCollectionId,
+                queries
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error ", error);
+            return false
+        }
+    }
+
+    // upload files services
+
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                config.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile :: error ", error);
+            return false
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            await this.bucket.deleteFile(
+                config.appwriteBucketId,
+                fileId
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite service :: deleteFile :: error ", error);
+            return false
+        }
+    }
+
+    getFilePreview(fileId){
+        try {
+            return this.bucket.getFilePreview(
+                config.appwriteBucketId,
+                fileId
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getFilePreview :: error ", error);
+            return false
+        }
+    }
+}
+
+const service = new Service()
+
+export default service
